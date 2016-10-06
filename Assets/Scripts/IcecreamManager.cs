c97900e176db0552e7e2b8429ac7e0c06a0c57e6
Ciprian Florescu <ciprianliis@gmail.com>
2016-10-06 13:44:29 +0200

    Added icecream manager. Yey!

---
+++ Assets/Scripts/IcecreamManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class IcecreamManager : MonoBehaviour
+{
+    private List<Icecream> icecreams = new List<Icecream>();
+
+    public void AddIcecream(Icecream icecream)
+    {
+        icecreams.Add(icecream);
+    }
+
+    public void RemoveIcecream(Icecream icecream)
+    {
+        icecreams.Remove(icecream);
+    }
+}
