ab7e20240d89aa35522bd6454cc655ee7f83c4da
Ciprian Florescu <ciprianliis@gmail.com>
2016-10-06 13:10:40 +0200

    TRex is in, rawr!

---
+++ Assets/TRex.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TRex : MonoBehaviour
+{
+    private enum Direction { LEFT, RIGHT }
+
+    private Direction dir = Direction.LEFT;
+
+    public KeyCode actionButton = KeyCode.Space;
+    public float speed = 4;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(actionButton))
+        {
+            ToggleDirection();
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        Vector3 vel = body.velocity;
+        float velX;
+        if (dir == Direction.LEFT)
+        {
+            velX = -speed;
+        }
+        else
+        {
+            velX = speed;
+        }
+        body.velocity = new Vector3(velX, vel.y, 0f);
+    }
+
+    public void ToggleDirection()
+    {
+        dir = (Direction) (((int)dir + 1) % 2);
+    }
+}
