2d967dbe61d389c80d307059cdeb3a8bf6fe06f6
Ciprian Florescu <ciprianliis@gmail.com>
2016-10-06 13:22:44 +0200

    Added folder structure, trex and icecream material. Added icecream script and prefab.

---
+++ Assets/Scripts/Icecream.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Icecream : MonoBehaviour
+{
+    public int pointValue = 1;
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
